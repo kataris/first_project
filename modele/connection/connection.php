7637084f0804b0123acdd5f16d36025630410885
kataris <mat29059542@gmail.com>
2014-02-12 20:30:04 +0100

    réunion du 12

--- modele/connection/connection.php
+++ modele/connection/connection.php
@@ -16,16 +16,20 @@
 		{
 			die('Erreur : ' . $e->getMessage());
 		}
-		$reponse = $bdd->query('SELECT mot_de_passe FROM utilisateur WHERE identifiant=\''.$pseudo.'\'');
-		while ($donnees = $reponse->fetch())
+		$reponse = $bdd->prepare('SELECT * FROM utilisateur where identifiant= BINARY"?"');
+		$reponse->execute(array($pseudo));
+		if($donnees = $reponse->fetch())
 		{
 			if($donnees['mot_de_passe']==sha1($mdp))
-			{
-				$content = $pseudo;
-			}else
-			{
-				$content = 'false';
-			}
+				{
+					$content = $pseudo;
+				}else
+				{
+					$content = 'false';
+				}
+		}else
+		{
+			$content = 'false';
 		}
 		
 		$reponse->closeCursor();
